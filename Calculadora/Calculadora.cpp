80d88a6cea415f58e7c4263169dea62bbd4e8bbc
Gary-Garibaldi <78573080+Gary-Garibaldi@users.noreply.github.com>
2022-09-28 19:33:36 -0500

    Create Calculadora.cpp

---
+++ Calculadora/Calculadora.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+
+using namespace std;
+
+float numero1;
+char a;
+float numero2;
+float resultado;
+
+
+int main() {
+    cout << "Este programa hace operaciones basicas entre dos numeros."<<endl;
+    cout << "Primer numero: ";
+    cin >> numero1;
+    cout << "Operacion: ";
+    cin >> a;
+    cout << "Segundo numero: ";
+    cin >> numero2;
+    if (a == '+') {
+        resultado = numero1+numero2;
+        cout<< "Resultado: \n" << numero1 << a << numero2 << "=" << resultado << endl;
+    }
+    if (a == '-') {
+        resultado = numero1-numero2;
+        cout<< "Resultado: \n" << numero1 << a << numero2 << "=" << resultado << endl;
+    }
+    if (a == '*') {
+        resultado = numero1*numero2;
+        cout<< "Resultado: \n" << numero1 << a << numero2 << "=" << resultado << endl;
+    }
+    if (a == '/') {
+        if (numero2==0){
+            cout<< "No es posible dividir entre cero."<<endl;
+        }
+        else {
+            resultado = numero1/numero2;
+            cout<< "Resultado: \n" << numero1 << a << numero2 << "=" << resultado << endl;
+        }
+    }
+    if (a == '%') {
+        resultado = (int)numero1 % (int)numero2;
+        cout<< "Resultado: \n" << numero1 << a << numero2 << "=" << resultado << endl;
+    }
+    return 0;
+}
